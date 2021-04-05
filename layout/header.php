5fe6113a394ad5184a90bfc21469c3d68a32f164
Dr-Deepak <deepak.sharmacpa@gmail.com>
2021-04-04 22:01:22 -0400

    udated bootstrap navbar

--- layout/header.php
+++ layout/header.php
@@ -5,39 +5,29 @@
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
 
     <!-- Font awsome CSS for Icons -->
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css"
-          rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+ 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
     <!--Override bootstrap style for footer -->
     <style>
-    header
-    {
-      background-color: #00000073;
-    }
-      footer
-      {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        height: 50px;
-        line-height: 60px;
-        background-color: #00000073;
-      }
+
     </style>
 </head>
 <body>
   <header>
-    <nav class="navbar">
-        <a href="default.php" class="navbar-brand" title=" homepage" ><?php echo $page_title;?></a>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <div class="container-fluid">      
+        <a href="#page-hero" class="navbar-brand" title=" homepage">
+            <i class ="fas fa-briefcase"></i> <?php echo $page_title;?>
+        </a>
 
-        <ul class="nav navbar-nav navbar-right">
+        <div class="navbar-nav">
           <?php
             if(session_status()== PHP_SESSION_ACTIVE){
                 session_start();
@@ -45,19 +35,20 @@
             }
             if(!empty($_SESSION['applicantID'])){
                  if(strcmp($_SESSION['position'], 'Admin')==0){
-                    echo '<li ><a href = "applicants.php" title = "View Applicants" > View Applicants </a ></li >';
+                    echo '<a class = "nav-item nav-link" href = "applicants.php" title = "View Applicants" > View Applicants </a>';
 
                   }
-                    echo' <li ><a href = "logout.php" title = "LOGOUT" > Log Out</a ></li >';
+                    echo' <a class = "nav-item nav-link" href = "logout.php" title = "LOGOUT" > Log Out</a >';
                 $_SESSION['noadmin']= "Notice you are not logged in as admin, so cannot view applicants page link";
             }
             else
             {
-                 echo '<li ><a href = "login.php" title = " View games" > LOG IN </a ></li>
-                       <li ><a href = "signup.php" title = " Add a game" > SIGN UP </a ></li>';
+                 echo '<a class = "nav-item nav-link" href = "login.php" title = " View games" > LOG IN </a>
+                       <a class = "nav-item nav-link" href = "signup.php" title = " Add a game" > SIGN UP </a> ';
             }
           ?>
-        </ul>
+        </div>
+      </div>
     </nav>
 </header>
 <main class="container">
